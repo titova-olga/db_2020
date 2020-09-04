f4e31bba96c35c3e28dc53b8e5ae51df3c60d7a3
Olga <olya11aprel1999@mail.ru>
2020-09-04 11:06:32 +0300

    Updated mail info implementation

--- src/main/java/homework/never_use_switch/mail_configuration/MailInfo.java
+++ src/main/java/homework/never_use_switch/mail_configuration/MailInfo.java
@@ -1,9 +1,6 @@
 package homework.never_use_switch.mail_configuration;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 /**
  * @author Evgeny Borisov
@@ -14,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MailInfo {
     private String text;
+    @NonNull
     private MailType mailType;
     private String email;
-
 }
